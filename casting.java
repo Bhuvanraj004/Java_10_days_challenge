6207baab9a7a42ac566c40b8cf8ae32832471d30
Bhuvanraj <144923204+Bhuvanraj004@users.noreply.github.com>
2025-01-22 19:01:24 +0530

    Update casting.java

--- casting.java
+++ casting.java
@@ -2,18 +2,17 @@ class alto{
     int display(int z,int y){
         return z+y;
     }
-    int display(int e,int q,int a){//return in numeral
-        return e*q*a;//void cant return value
+    int display(int e,int q,int a){
+        return e*q*a;
     }
 }
 class casting{
     public static void main(String[] args){
         int a=1234;
-        float num=a;//implicit
+        float num=a;
         System.out.println(num);
         int e=34;
-        double n=(double)e;//explicit casting
+        double n=(double)e;
         System.out.println(n);
     }
 }
-//overloading
