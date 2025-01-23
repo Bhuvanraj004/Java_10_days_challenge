84faba9fcc670af2acfba77e2a66731b70057926
Bhuvanraj <144923204+Bhuvanraj004@users.noreply.github.com>
2025-01-23 16:09:54 +0530

    updated

---
+++ binarysearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class binarysearch{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int arr[]=new int[size];
+    for(int i=0;i<size;i++){
+        arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        for(int i=0;i<size;i++){
+            if(arr[i]==a){
+                System.out.println("Element is present at index "+i);
+            break;
+            }
+        }
+  }    
+}
